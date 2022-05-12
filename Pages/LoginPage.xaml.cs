e486325fd359331c7f95ef31058f3aa1829930f7
Rixweys <82574402+Rixweys@users.noreply.github.com>
2022-05-12 12:14:05 +0300

    Initial commit

---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using DemoTranning.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace DemoTranning.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Pages.RegisterPage());
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=192.168.4.211; Initial Catalog=dfominaDEMO; user=MSSQL207; password=12345");
+                SqlCommand sqlCommand = new SqlCommand($@"select * from User where UserLogin = '{LoginTextBox.Text}' and Password= '{PassowrdTextBox.Password}'", conn);
+                conn.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    Manager.MainFrame.Navigate(new Pages.ProductsPage());
+                }
+                else
+                {
+                    MessageBox.Show("Неверные данные!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка!\n" + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
